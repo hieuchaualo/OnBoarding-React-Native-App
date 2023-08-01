23c1829be7cc5033fdd470d81caf3043c8a9c02e
hieuchaualo <chau.huynh190205@vnuk.edu.vn>
2023-08-01 22:15:40 +0700

    Update login screen

--- screens/login/LoginScreen.tsx
+++ screens/login/LoginScreen.tsx
@@ -1,12 +1,10 @@
 import {
-  SafeAreaView,
-  StyleSheet,
+  ScrollView,
   Text,
-  TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
-import React, { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import Spacing from "../../constants/Spacing";
 import FontSize from "../../constants/FontSize";
 import Colors from "../../constants/Colors";
@@ -14,28 +12,53 @@ import { Ionicons } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../navigation";
 import AppTextInput from "../../components/AppTextInput";
-import { loginAccount } from "../../api";
+import { getAccount, loginAccount } from "../../api";
 import { getItemAsync, setItemAsync } from "expo-secure-store";
 import { regexEmail } from "../../constants/regexPattern";
 import { AxiosResponse } from "axios";
-import { ThemeFonts } from "../../constants";
+import { ThemeColors, ThemeDimensions, ThemeFonts, ThemeStyles } from "../../constants";
+import { Button } from "../../components";
 
-type Props = NativeStackScreenProps<RootStackParamList, "Login">;
+type LoginScreenProps = NativeStackScreenProps<RootStackParamList, "Login">;
 
-const LoginScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
+const LoginScreen: FC<LoginScreenProps> = ({ navigation: { navigate } }) => {
   const [email, setEmail] = useState("");
-  const [isEmailNotValidated, setisEmailNotValidated] = useState(false);
+  const [isEmailNotValidated, setIsEmailNotValidated] = useState(false);
   const [password, setPassword] = useState("");
-  const [isPasswordNotValidated, setisPasswordNotValidated] = useState(false);
+  const [isPasswordNotValidated, setIsPasswordNotValidated] = useState(false);
+
+  useEffect(() => {
+    const fetchAccount = async () => {
+      try {
+        const token = await getItemAsync("secure_token")
+        if (token) {
+          const response: AxiosResponse<any, any> = await getAccount();
+          if (response?.status === 200) {
+            const responseData = response.data.data
+            const account = JSON.stringify({
+              username: responseData.name,
+              email: responseData.email,
+              id: responseData._id,
+            });
+            await setItemAsync("account", account);
+            navigate("Home")
+          }
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchAccount()
+  }, [])
 
   const checkEmailInvalid = () => {
-    setisEmailNotValidated(!regexEmail.test(email));
+    setIsEmailNotValidated(!regexEmail.test(email));
     return !regexEmail.test(email);
   };
 
-  const checkPasswordlInvalid = () => {
-    setisPasswordNotValidated(!(password.length >= 8));
-    return !(password.length >= 8);
+  const checkPasswordInvalid = () => {
+    setIsPasswordNotValidated(password.length < 8);
+    return password.length < 8;
   };
 
   const login = async () => {
@@ -59,212 +82,173 @@ const LoginScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
     }
   };
 
+  const handleLoginPress = async () => {
+    const emailInvalid = checkEmailInvalid();
+    const passwordInvalid = checkPasswordInvalid();
+    if (emailInvalid || passwordInvalid) { }
+    else {
+      const isLoginSuccess = await login();
+      if (isLoginSuccess) navigate("Home");
+    }
+  }
+
   return (
-    <SafeAreaView>
-      <View
-        style={{
-          padding: Spacing * 2,
-        }}
-      >
-        <View
-          style={{
-            alignItems: "center",
-          }}
-        >
-          <Text
-            style={{
-              fontSize: FontSize.xLarge,
-              color: Colors.primary,
-              fontFamily: ThemeFonts.bold,
-              marginVertical: Spacing * 3,
-            }}
-          >
-            Login here
-          </Text>
-          <Text
-            style={{
-              fontFamily: ThemeFonts.semiBold,
-              fontSize: FontSize.large,
-              maxWidth: "80%",
-              textAlign: "center",
-            }}
-          >
-            Welcome back you've been missed!
-          </Text>
-        </View>
-        <View
-          style={{
-            marginVertical: Spacing * 3,
-          }}
-        >
-          <AppTextInput
-            value={email}
-            onChangeText={setEmail}
-            placeholder="Email"
-          />
-          {isEmailNotValidated && (
-            <Text
-              style={{
-                fontFamily: ThemeFonts.regular,
-                fontSize: FontSize.small,
-                color: "#F00",
-                alignSelf: "flex-start",
-              }}
-            >
-              Your Email is invalid. Please enter again.
-            </Text>
-          )}
-          <AppTextInput
-            value={password}
-            onChangeText={setPassword}
-            placeholder="Password"
-            secureTextEntry
-          />
-          {isPasswordNotValidated && (
-            <Text
-              style={{
-                fontFamily: ThemeFonts.regular,
-                fontSize: FontSize.small,
-                color: "#F00",
-                alignSelf: "flex-start",
-              }}
-            >
-              Your Password is invalid. Please enter again.
-            </Text>
-          )}
-        </View>
-
-        <TouchableOpacity onPress={() => navigate("ForgotPassword")}>
-          <Text
-            style={{
-              fontFamily: ThemeFonts.semiBold,
-              fontSize: FontSize.small,
-              color: Colors.primary,
-              alignSelf: "flex-end",
-            }}
-          >
-            Forgot your password ?
-          </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={async () => {
-            const emailInvalid = checkEmailInvalid();
-            const passwordInvalid = checkPasswordlInvalid();
-            if (emailInvalid || passwordInvalid) { }
-            else {
-              const isLoginSuccess = await login();
-              if (isLoginSuccess) navigate("Home");
-            }
-          }}
-          style={{
-            padding: Spacing * 1.5,
-            backgroundColor: Colors.primary,
-            marginVertical: Spacing * 3,
-            borderRadius: Spacing,
-            shadowColor: Colors.primary,
-            shadowOffset: {
-              width: 0,
-              height: Spacing,
-            },
-            shadowOpacity: 0.3,
-            shadowRadius: Spacing,
-          }}
-        >
-          <Text
-            style={{
-              fontFamily: ThemeFonts.bold,
-              color: Colors.onPrimary,
-              textAlign: "center",
-              fontSize: FontSize.large,
-            }}
-          >
-            Login
-          </Text>
-        </TouchableOpacity>
+    <ScrollView style={{ padding: ThemeDimensions.positive3 }}>
+      <View style={{ alignItems: "center" }}>
+        <Text style={ThemeStyles.h1}>
+          Login here
+        </Text>
         <Text
           style={{
-            fontFamily: ThemeFonts.regular,
+            ...ThemeStyles.h2,
+            fontFamily: ThemeFonts.semiBold,
+            color: ThemeColors.dark,
+          }}
+        >
+          Welcome back you've been missed!
+        </Text>
+      </View>
+
+      <AppTextInput
+        value={email}
+        onChangeText={setEmail}
+        placeholder="Email"
+      />
+      {isEmailNotValidated && (
+        <Text style={{
+          fontFamily: ThemeFonts.regular,
+          fontSize: ThemeDimensions.fontSize.sm,
+          color: ThemeColors.danger,
+          alignSelf: "flex-start",
+        }}>
+          Your Email is invalid. Please enter again.
+        </Text>
+      )}
+
+      <AppTextInput
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Password"
+        secureTextEntry
+      />
+      {isPasswordNotValidated && (
+        <Text style={{
+          fontFamily: ThemeFonts.regular,
+          fontSize: ThemeDimensions.fontSize.sm,
+          color: ThemeColors.danger,
+          alignSelf: "flex-start",
+        }}>
+          Your Password is invalid. Please enter again.
+        </Text>
+      )}
+
+      <TouchableOpacity
+      onPress={() => navigate("ForgotPassword")}
+      style={{ marginTop: ThemeDimensions.positive2}}
+      >
+        <Text
+          style={{
+            fontFamily: ThemeFonts.semiBold,
+            fontSize: ThemeDimensions.fontSize.sm,
+            color: ThemeColors.primary,
+            alignSelf: "flex-end",
+          }}
+        >
+          Forgot your password ?
+        </Text>
+      </TouchableOpacity>
+
+      <Button
+        title="Login"
+        style={{
+          height: ThemeDimensions.positive10,
+          marginVertical: ThemeDimensions.positive5
+        }}
+        onPress={handleLoginPress}
+      />
+
+      <Text
+        style={{
+          fontFamily: ThemeFonts.regular,
+          color: ThemeColors.dark,
+          textAlign: "center",
+          fontSize: ThemeDimensions.fontSize.sm,
+        }}
+      >
+        Don't have an account?
+      </Text>
+      <TouchableOpacity
+        onPress={() => navigate("Register")}
+        style={{
+          padding: Spacing,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: ThemeFonts.semiBold,
+            color: Colors.primary,
+            textAlign: "center",
+            fontSize: FontSize.small,
+          }}
+        >
+          Sign up
+        </Text>
+      </TouchableOpacity>
+
+      <View
+        style={{
+          marginVertical: Spacing * 3,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: ThemeFonts.semiBold,
             color: Colors.text,
             textAlign: "center",
             fontSize: FontSize.small,
           }}
         >
-          Don't have an account ?
+          Or continue with
         </Text>
-        <TouchableOpacity
-          onPress={() => navigate("Register")}
-          style={{
-            padding: Spacing,
-          }}
-        >
-          <Text
-            style={{
-              fontFamily: ThemeFonts.semiBold,
-              color: Colors.primary,
-              textAlign: "center",
-              fontSize: FontSize.small,
-            }}
-          >
-            Sign up
-          </Text>
-        </TouchableOpacity>
 
         <View
           style={{
-            marginVertical: Spacing * 3,
+            marginTop: Spacing,
+            flexDirection: "row",
+            justifyContent: "center",
           }}
         >
-          <Text
+          <TouchableOpacity
             style={{
-              fontFamily: ThemeFonts.semiBold,
-              color: Colors.text,
-              textAlign: "center",
-              fontSize: FontSize.small,
+              padding: Spacing,
+              backgroundColor: Colors.gray,
+              borderRadius: Spacing / 2,
+              marginHorizontal: Spacing,
             }}
           >
-            Or continue with
-          </Text>
-
-          <View
+            <Ionicons
+              name="logo-google"
+              color={Colors.text}
+              size={Spacing * 2}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
             style={{
-              marginTop: Spacing,
-              flexDirection: "row",
-              justifyContent: "center",
+              padding: Spacing,
+              backgroundColor: Colors.gray,
+              borderRadius: Spacing / 2,
+              marginHorizontal: Spacing,
             }}
           >
-            <TouchableOpacity
-              style={{
-                padding: Spacing,
-                backgroundColor: Colors.gray,
-                borderRadius: Spacing / 2,
-                marginHorizontal: Spacing,
-              }}
-            >
-              <Ionicons
-                name="logo-google"
-                color={Colors.text}
-                size={Spacing * 2}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{
-                padding: Spacing,
-                backgroundColor: Colors.gray,
-                borderRadius: Spacing / 2,
-                marginHorizontal: Spacing,
-              }}
-            >
-              <Ionicons
-                name="logo-facebook"
-                color={Colors.text}
-                size={Spacing * 2}
-              />
-            </TouchableOpacity>
-          </View>
+            <Ionicons
+              name="logo-facebook"
+              color={Colors.text}
+              size={Spacing * 2}
+            />
+          </TouchableOpacity>
         </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 
