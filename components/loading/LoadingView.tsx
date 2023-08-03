1e9a8f7be62f178706806af27c0d8b5d4ea4df3f
hieuchaualo <chau.huynh190205@vnuk.edu.vn>
2023-08-03 21:45:08 +0700

    Update loading view

--- components/loading/LoadingView.tsx
+++ components/loading/LoadingView.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { ActivityIndicator, Text, View } from "react-native"
-import Colors from "../../constants/Colors"
+import { ThemeColors, ThemeDimensions } from "../../constants"
 
 const LoadingView = () => {
     return (
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-            <ActivityIndicator size='large' color={Colors.primary} />
-            <Text style={{ fontSize: 32, color: Colors.primary, }}>
+        <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <ActivityIndicator size='large' color={ThemeColors.primary} />
+            <Text style={{ fontSize: ThemeDimensions.fontSize.xl, color: ThemeColors.primary, }}>
                 { } Loading
             </Text>
         </View>
