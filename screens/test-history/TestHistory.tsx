8b2dbd9ea48a62e084d4fd367a069ff4af1ba6ab
hieuchaualo <chau.huynh190205@vnuk.edu.vn>
2023-08-12 23:56:37 +0700

    Update Test history chart

--- screens/test-history/TestHistory.tsx
+++ screens/test-history/TestHistory.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -12,6 +12,7 @@ import { fromSecondToDateTime } from '../../utils';
 import { RootStackName, ThemeColors, ThemeDimensions, ThemeFonts, ThemeStyles } from '../../constants';
 import { BottomNav, Column, LoadingView, Row } from '../../components';
 import { RootStackParamList } from '../../types';
+import { PieChart } from 'react-native-chart-kit';
 
 const iconStyle = {
   padding: ThemeDimensions.positive2,
@@ -19,55 +20,81 @@ const iconStyle = {
   borderBottomLeftRadius: ThemeDimensions.positive1,
 }
 
-function getIconByScore(score: number, timing: number) {
+const FrownIcon = (): ReactNode => <View style={{
+  ...iconStyle,
+  backgroundColor: ThemeColors.pinkLight,
+}}>
+  <FeatherIcon name="frown" size={ThemeDimensions.positive8} color={ThemeColors.danger} />
+</View>
+
+const MehIcon = (): ReactNode => <View style={{
+  ...iconStyle,
+  backgroundColor: ThemeColors.yellowLight,
+}}>
+  <FeatherIcon name="meh" size={ThemeDimensions.positive8} color={ThemeColors.warning} />
+</View>
+
+const SmellIcon = (): ReactNode => <View style={{
+  ...iconStyle,
+  backgroundColor: '#ceecce',
+}}>
+  <FeatherIcon name="smile" size={ThemeDimensions.positive8} color={ThemeColors.teal} />
+</View>
+
+
+const iconByScore = {
+  excellent: SmellIcon,
+  ordinary: MehIcon,
+  fail: FrownIcon,
+}
+
+function getRankingByScore(score: number, timing: number) {
   if (score > 1.0) return (
-    <View style={{
-      ...iconStyle,
-      backgroundColor: ThemeColors.pinkLight,
-    }}>
-      <FeatherIcon name="frown" size={ThemeDimensions.positive8} color={ThemeColors.danger} />
-    </View>
+    'fail'
   )
+
   if (score >= 0.8) {
     if (timing < 0.8) return (
-      <View style={{
-        ...iconStyle,
-        backgroundColor: '#ceecce',
-      }}>
-        <FeatherIcon name="smile" size={ThemeDimensions.positive8} color={ThemeColors.teal} />
-      </View>
+      'excellent'
     )
     if (timing < 1.0) return (
-      <View style={{
-        ...iconStyle,
-        backgroundColor: ThemeColors.yellowLight,
-      }}>
-        <FeatherIcon name="meh" size={ThemeDimensions.positive8} color={ThemeColors.warning} />
-      </View>
+      'ordinary'
     )
   }
 
   if (score >= 0.65) {
     if (timing < 1.0) return (
-      <View style={{
-        ...iconStyle,
-        backgroundColor: ThemeColors.yellowLight,
-      }}>
-        <FeatherIcon name="meh" size={ThemeDimensions.positive8} color={ThemeColors.warning} />
-      </View>
+      'ordinary'
     )
   }
 
   return (
-    <View style={{
-      backgroundColor: ThemeColors.pinkLight,
-      padding: ThemeDimensions.positive2,
-      borderTopLeftRadius: ThemeDimensions.positive1,
-      borderBottomLeftRadius: ThemeDimensions.positive1,
-    }}>
-      <FeatherIcon name="frown" size={ThemeDimensions.positive8} color={ThemeColors.danger} />
-    </View>
+    'fail'
   )
+}
+
+type TRanking = {
+  excellent: number;
+  ordinary: number;
+  fail: number;
+}
+
+const getRanking = (miniTestHistoryList: IMiniTestHistory[]): TRanking => {
+  const ranking = {
+    excellent: 0,
+    ordinary: 0,
+    fail: 0,
+  }
+
+  miniTestHistoryList.forEach(miniTestHistory => {
+    const rankingNamed = getRankingByScore(
+      Number(miniTestHistory.totalCorrectAnswers) / Number(miniTestHistory.totalQuestions),
+      Number(miniTestHistory.timeTaken) / Number(miniTestHistory.timeLimit),
+    )
+    ranking[rankingNamed] += 1
+  })
+
+  return ranking
 }
 
 type TestHistoryProps = NativeStackScreenProps<RootStackParamList, "TestHistory">;
@@ -75,6 +102,7 @@ type TestHistoryProps = NativeStackScreenProps<RootStackParamList, "TestHistory"
 const TestHistory: FC<TestHistoryProps> = ({ navigation }) => {
   const [miniTestHistoryList, setMiniTestHistoryList] = useState<IMiniTestHistory[]>();
   const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [pieChartData, setPieChartData] = useState<TRanking>()
 
   useEffect(() => {
     const fetchMiniTestById = async () => {
@@ -84,6 +112,8 @@ const TestHistory: FC<TestHistoryProps> = ({ navigation }) => {
           const miniTestHistoryData: IMiniTestHistory[] = response.data.data
           setMiniTestHistoryList(miniTestHistoryData);
           setIsLoading(false)
+          const ranking = getRanking(miniTestHistoryData)
+          setPieChartData(ranking)
         }
       } catch (error) {
         console.error(error)
@@ -100,10 +130,59 @@ const TestHistory: FC<TestHistoryProps> = ({ navigation }) => {
           Mini test history
         </Text>
       </View>
+
       <ScrollView>
+        {pieChartData && <Row>
+          <Column style={{ paddingTop: ThemeDimensions.positive1, }}>
+            <PieChart
+              data={[
+                {
+                  name: "Excellent",
+                  ranking: pieChartData.excellent,
+                  color: ThemeColors.success,
+                  legendFontColor: "#7F7F7F",
+                  legendFontSize: 15
+                },
+                {
+                  name: "Ordinary",
+                  ranking: pieChartData.ordinary,
+                  color: ThemeColors.warning,
+                  legendFontColor: "#7F7F7F",
+                  legendFontSize: 15
+                },
+                {
+                  name: "Fail",
+                  ranking: pieChartData.fail,
+                  color: ThemeColors.danger,
+                  legendFontColor: "#7F7F7F",
+                  legendFontSize: 15
+                },
+              ]}
+              width={ThemeDimensions.windowWidth - ThemeDimensions.positive4}
+              height={ThemeDimensions.windowWidth50}
+              accessor={"ranking"}
+              backgroundColor={ThemeColors.white}
+              chartConfig={{
+                decimalPlaces: 0, // optional, defaults to 2dp
+                color: () => ThemeColors.warning,
+                labelColor: () => ThemeColors.primary,
+                backgroundGradientFrom: ThemeColors.white,
+                backgroundGradientTo: ThemeColors.white,
+                propsForDots: { r: ThemeDimensions.positive1 },
+              }}
+              paddingLeft={"0"}
+              style={{
+                margin: ThemeDimensions.positive1,
+                backgroundColor: ThemeColors.white,
+                borderRadius: ThemeDimensions.positive1,
+              }}
+            />
+          </Column>
+        </Row>}
+
         <View style={{ padding: ThemeDimensions.positive1, paddingTop: 0, }}>
           {isLoading
-            ? <LoadingView />
+            ? <Column style={{ height: ThemeDimensions.windowHeight75 }}><LoadingView /></Column>
             : miniTestHistoryList?.map((miniTestHistory, miniTestHistoryIndex) => (
               <Row
                 key={miniTestHistory._id ?? miniTestHistoryIndex}
@@ -114,10 +193,10 @@ const TestHistory: FC<TestHistoryProps> = ({ navigation }) => {
                 }}
               >
                 <Column style={{ flex: undefined }}>
-                  {getIconByScore(
+                  {iconByScore[getRankingByScore(
                     Number(miniTestHistory.totalCorrectAnswers) / Number(miniTestHistory.totalQuestions),
                     Number(miniTestHistory.timeTaken) / Number(miniTestHistory.timeLimit),
-                  )}
+                  )]()}
                 </Column>
 
                 <Column style={{ paddingHorizontal: ThemeDimensions.positive2, }}>
